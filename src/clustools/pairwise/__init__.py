78787f5a8a6a4c354fa2c90c720de49f76e034d3
psolsfer <23430901+psolsfer@users.noreply.github.com>
2025-09-30 14:54:13 +0900

    refactor: update __init__ of pairwise module

--- src/clustools/pairwise/__init__.py
+++ src/clustools/pairwise/__init__.py
@@ -1,7 +1,13 @@
 """Comparison of multiple clustering results."""
 
 from clustools.pairwise.agreement import coassociation_matrix, pairwise_ari_matrix
-from clustools.pairwise.distances import overlap_distance_matrix
+from clustools.pairwise.distances import cluster_similarity_matrix, overlap_distance_matrix
 from clustools.pairwise.summary import ari_summary
 
-__all__ = ["ari_summary", "coassociation_matrix", "overlap_distance_matrix", "pairwise_ari_matrix"]
+__all__ = [
+    "ari_summary",
+    "cluster_similarity_matrix",
+    "coassociation_matrix",
+    "overlap_distance_matrix",
+    "pairwise_ari_matrix",
+]
