85521a66948eec71639680133b1275f9cfd1084c
psolsfer <23430901+psolsfer@users.noreply.github.com>
2025-12-26 19:14:45 +0900

    bump(release): v0.3.3 → v0.3.4

--- src/clustools/__init__.py
+++ src/clustools/__init__.py
@@ -5,4 +5,4 @@ A lightweight Python package providing essential clustering utilities for cluste
 
 __author__ = """Pablo Solís-Fernández"""
 __email__ = "psolsfer@gmail.com"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
